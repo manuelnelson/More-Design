e2fe7389ab87a522a9adcf700e143da52f3754e2
manuelnelson <elnels@gmail.com>
2017-05-05 07:18:03 -0500

    use cloudinary for home

--- models/Home.js
+++ models/Home.js
@@ -13,18 +13,11 @@ var Home = new keystone.List('Home', {
 	map: { name: 'name' },
 	autokey: { path: 'slug', from: 'name', unique: true }
 });
-var storage = new keystone.Storage({
-	adapter: keystone.Storage.Adapters.FS,
-	fs: {
-		path: keystone.expandPath('./public/files'),
-		publicPath: '/public/files/',
-	}
-});
 Home.add({
 	name: { type: String, required: true },
-	backgroundImage1: { type: Types.File, storage:storage },
-	backgroundImage2: { type: Types.File, storage:storage },
-	backgroundImage3: { type: Types.File, storage:storage }
+	backgroundImage1: { type: Types.CloudinaryImage },
+	backgroundImage2: { type: Types.CloudinaryImage },
+	backgroundImage3: { type: Types.CloudinaryImage }
 });
 
 Home.defaultColumns = 'name';
