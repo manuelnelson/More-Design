2eedf2af74b1acd5b68097829bd64a350d36335a
manuelnelson <elnels@gmail.com>
2017-05-05 08:25:55 -0500

    update keystone navigation

--- keystone.js
+++ keystone.js
@@ -81,7 +81,6 @@ keystone.set('routes', require('./routes'));
 // Configure the navigation bar in Keystone's Admin UI
 
 keystone.set('nav', {
-	'users': 'users'
 });
 
 // Start Keystone to connect to your database and initialise the web server
