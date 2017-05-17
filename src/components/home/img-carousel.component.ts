7833478d766198881ba94ad495fe5c897908dccb
manuelnelson <elnels@gmail.com>
2017-05-16 21:37:00 -0500

    responsive fixes

--- src/components/home/img-carousel.component.ts
+++ src/components/home/img-carousel.component.ts
@@ -32,7 +32,7 @@ export class ImageCarouselComponent implements OnInit {
                 this.imageNdx = 0;
                 return;
             }
-        }, 5000);
+        }, 500000);
     }
 
 }
