fb0fa6f7ebd04dd105a5af80d0cd179cfb8c7aaf
manuelnelson <elnels@gmail.com>
2017-05-21 16:06:21 -0500

    update password

--- models/User.js
+++ models/User.js
@@ -11,7 +11,7 @@ var User = new keystone.List('User');
 User.add({
 	name: { type: Types.Name, required: true, index: true },
 	email: { type: Types.Email, initial: true, required: true, index: true },
-	password: { type: Types.Password, initial: true, required: true, select:false }
+	password: { type: Types.Password, initial: true, required: true }
 }, 'Permissions', {
 	isAdmin: { type: Boolean, label: 'Can access Keystone', index: true }
 });
