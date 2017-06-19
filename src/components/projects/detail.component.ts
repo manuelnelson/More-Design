69fcc07c5e5f992d62a42ed54981eaf1f550cc2d
manuelnelson <elnels@gmail.com>
2017-06-18 21:41:50 -0500

    prerender wait

--- src/components/projects/detail.component.ts
+++ src/components/projects/detail.component.ts
@@ -3,7 +3,7 @@ import { RestService } from '../../services/rest.service';
 import { Project, ProjectType, Slide } from '../../models';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Meta } from '@angular/platform-browser';
-
+declare var window: any;
 @Component({
     template: require('./detail.component.html')
 })
@@ -73,46 +73,9 @@ export class ProjectDetailComponent implements OnInit{
           name: "twitter:image",
           content: this.project.thumbnailImage.url
         })
+        window.prerenderReady = true;
     }
     ngOnInit() {
-      // this.project = this.route.snapshot.data['project'];
-      // let siteTag = " | More Design";
-      // this.meta.addTag({
-      //   property: "og:url",
-      //   name: "url",
-      //   content: window.location.href
-      // })
-      // this.meta.addTag({
-      //   property: "og:site_name",
-      //   content: "More Design"
-      // })
-      //
-      // if(this.project.title){
-      //   this.meta.addTag({
-      //     name: "title",
-      //     content: this.project.title + siteTag
-      //   })
-      //   this.meta.addTag({
-      //     property: "og:title",
-      //     name: "title",
-      //     content: this.project.title + siteTag
-      //   })
-      // }
-      // if(this.project.metaDescription){
-      //   this.meta.addTag({
-      //     name: "description",
-      //     content: this.project.metaDescription
-      //   })
-      //   this.meta.addTag({
-      //     name: "og:description",
-      //     content: this.project.metaDescription
-      //   })
-      // }
-      // this.meta.addTag({
-      //   property: "og:image",
-      //   name: "image",
-      //   content: this.project.thumbnailImage.url
-      // })
     }
     toggleDescription(){
         this.showDescription = !this.showDescription;
